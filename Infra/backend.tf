4a68c68172d0a477da289f850ae7b5b2e4346d4f
jujugitlab23 <linweijianjulian@gmail.com>
2023-09-03 19:02:19 +0800

    Updated S3 bucket policy

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,4 +1,12 @@
 terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
   backend "s3" {
     bucket = "group1-terrastate-bucket"
     key    = "group1-terrastate-s3.tfstate"
