644701bdc96e4c018de77f236a929e90a037326f
jujugitlab23 <linweijianjulian@gmail.com>
2023-09-03 19:17:40 +0800

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,4 +1,5 @@
 terraform {
+/* Comment for remote deployment
 
   required_providers {
     aws = {
@@ -6,7 +7,7 @@ terraform {
       version = "~> 4.18.0"
     }
   }
-
+*/
   backend "s3" {
     bucket = "group1-terrastate-bucket"
     key    = "group1-terrastate-s3.tfstate"
